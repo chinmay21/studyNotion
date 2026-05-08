3362865b78c6cd578a158cf51dd895d88860e54e
chinmay21 <137067038+chinmay21@users.noreply.github.com>
2026-05-09 00:57:52 +0530

    Update mailSender to use Gmail SMTP settings
    Change mail transport host to Gmail SMTP.

--- server/utils/mailSender.js
+++ server/utils/mailSender.js
@@ -4,7 +4,9 @@ const mailSender = async (email, title, body) => {
     try {
 
         let transporter = nodemailer.createTransport({
-            host: process.env.MAIL_HOST,
+            host: "smtp.gmail.com",
+            port: 465,
+            secure: true,
             auth: {
                 user: process.env.MAIL_USER,
                 pass: process.env.MAIL_PASS,
